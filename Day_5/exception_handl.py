a8637656be3324c6b7da0c86bc81d0d850f8642a
Aamin Simmi Singh <aaminsimmisingh@gmail.com>
2026-06-30 20:42:19 +0530

    modifying the existing files

---
+++ Day_5/exception_handl.py
@@ -0,0 +1,12 @@
+num = int("ram")#Value Error
+print(10/0)#NameError ----x not defined
+print(x)
+#Type of Built in Error in Python
+
+
+
+IndentationError
+NameError
+TypeError
+ValueError
+ZeroDivisionError
--- Day_5/file_handle.py
+++ Day_5/file_handle.py
@@ -1,11 +1,20 @@
 with open("class.txt", "w") as file:
     file.write("Good morning class!\n")
     file.write("Hello everyone, welcome to the class!\n", )
+    file.write(f"Karan, 23,Pune\n")
 
 
-with open("batch.txt","r") as f:
+with open("class.txt","r") as file:
 
-    content = file.read
+    content = file.read()
     print(content)
 
+
+with open("class.txt","r") as file:
+    for line in file:
+        print(line.strip())
+
+with open("class.txt",'r') as file:
+     first_line = file.readline 
+     print(first_line)  
     
