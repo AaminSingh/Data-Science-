0edce41141879fdb83c2acbf8b680a5bbeb8fa41
Aamin Simmi Singh <aaminsimmisingh@gmail.com>
2026-06-30 11:48:04 +0530

    pls resolve commits

--- Day_5/file_handle.py
+++ Day_5/file_handle.py
@@ -1,3 +1,11 @@
 with open("class.txt", "w") as file:
     file.write("Good morning class!\n")
     file.write("Hello everyone, welcome to the class!\n", )
+
+
+with open("batch.txt","r") as f:
+
+    content = file.read
+    print(content)
+
+    
