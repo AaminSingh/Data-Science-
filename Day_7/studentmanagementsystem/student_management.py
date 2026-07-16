833a849440cb56d7a466ae285ff0b928e4b9a4a6
Aamin Simmi Singh <aaminsimmisingh@gmail.com>
2026-07-16 16:44:20 +0530

    updating the files

--- Day_7/studentmanagementsystem/student_management.py
+++ Day_7/studentmanagementsystem/student_management.py
@@ -1,49 +1,74 @@
-while True:
-    print("====== MENU ======")
-    def add_student():
-      print("1. Add Student")
-    def view_student():  
-     print("2. View Student")
-    def search_student(): 
-     print("3. Search Student")
-    def update_student(): 
-      print("4. Update Student")
-    def delete_student():  
-      print("5. Delete Student")
-    print("6. Exit")
+# ###STUDENT CLASS ####
+# class Student:
+#    def __init__(self,name,age,course,RollNumber):
+#         self.name = name
+#         self.age = age
+#         self.course = course
+#         self.RollNumber = RollNumber
 
-    choice = input("Enter Choice: ")
+# new_student = Student("Bisht",21,"B.tech",1201) 
+# print("\nStudent Object Created Successfully")
+# print(new_student.__dict__)    
 
-    if choice == '1':
-       add_student()
-    elif choice == '2':
-        view_student()
+
+
+# def add_student():
+#       print("1. Add Student")
+# def view_student():  
+#      print("2. View Student")
+# def search_student(): 
+#      print("3. Search Student")
+# def update_student(): 
+#       print("4. Update Student")
+# def delete_student():  
+#       print("5. Delete Student")
+# print("6. Exit")
+
+
+# while True:
+#     print("====== MENU ======")
+#     print("1. Add Student")
+#     print("2. View Student")
+#     print("3. Search Student")
+#     print("4. Update Student")
+#     print("5. Delete Student")
+#     print("6. Exit")
+
+#     choice = input("Enter Choice: ")
+
+#     if choice == '1':
+#        add_student()
+#     elif choice == '2':
+#         view_student()
         
-    elif choice == '3':
-        search_student()
+#     elif choice == '3':
+#         search_student()
         
-    elif choice == '4':
-       update_student()
+#     elif choice == '4':
+#        update_student()
 
-    elif choice == '5':
-       delete_student()
+#     elif choice == '5':
+#        delete_student()
 
-    elif choice == '6':
-       print("Exiting...")
-       break
-    else:
-        print("Invalid choice. Please try again.")  
+#     elif choice == '6':
+#        print("Exiting...")
+#        break
+#     else:
+#         print("Invalid choice. Please try again.")  
 
 
-class Student:
-   def __init__(self,name,age,course,RollNumber):
-        self.name = name
-        self.age = age
-        self.course = course
-        self.RollNumber = RollNumber
+#To establish a connection
+#between Python and the file.
+with open("student.txt","w") as file:
+    file.write("Hello World\n")
+    file.write("Hello\n")
+    file.write("python\n")
+    file.write("ABC\n")
+    file.write("XYZ\n")
 
-new_student = Student("Bisht",21,"B.tech",1201) 
-print(new_student.__dict__)    
+
+
+
 
         
         
